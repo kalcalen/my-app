5f0a9221ac0d490b56599947e9453dbae994f599
kalcalen <kalcalen@gmail.com>
2020-07-09 12:28:28 -0700

    added prettier, filled routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react"
-import "./style/master.scss"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React from "react";
+import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import EditDetail from "./pages/EditDetail";
+import Parks from "./pages/Parks";
+import Players from "./pages/Players";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route exact path="/" component={} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  )
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/landing" component={Landing} />
+            <Route exact path="/edit-detail" component={EditDetail} />
+            <Route exact path="/Parks" component={Parks} />
+            <Route exact path="/Players" component={Players} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
+   );
 }
 
-export default App
+export default App;
