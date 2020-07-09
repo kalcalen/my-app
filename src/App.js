7b77a5e01b5b7ce0fd303cd58feee81e5aac4814
kalcalen <kalcalen@gmail.com>
2020-07-09 11:44:23 -0700

    transferred style folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import "./style/master.scss"
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <h1 className="text-danger">Hello World</h1>
+    </div>
+  )
 }
 
 export default App
