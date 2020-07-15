7f848c677f8512b1ba4a567fb7ef5580a6f7d1ae
kalcalen <kalcalen@gmail.com>
2020-07-14 21:51:18 -0700

    changed background

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,7 +12,6 @@ export default function Landing() {
                      <div className="card mb-5">
                         <div className="card-body landing-text">
                            <h2 className="card-title">Nice to meet you</h2>
-                           <p className="mb-2">Sign up for White Bear.</p>
                            <p className="mb-3">Free forever.</p>
                            <button
                               id="sign-up"
@@ -54,43 +53,6 @@ export default function Landing() {
                                  Lets go!
                               </a>
                            </div>
-                        </div>
-                     </div>
-                  </div>
-
-                  <div className="offset-1 col-10 offset-sm-2 col-sm-8 offset-md-1 col-md-5 offset-lg-1 col-lg-4 offset-xl-1 col-xl-5">
-                     <div className="card mb-5">
-                        <div className="card-body landing-text">
-                           <h2 className="card-title">Welcome Back</h2>
-                           <p className="mb-3">
-                              Log-in with your email address and password.
-                           </p>
-                           <form className="form-group">
-                              <label className="text-muted" for="email">
-                                 Email Address
-                              </label>
-                              <input
-                                 className="form-control"
-                                 type="email"
-                                 id="emailWelcome"
-                                 name="email"
-                              />
-                              <label className="text-muted" for="password">
-                                 Password
-                              </label>
-                              <input
-                                 className="form-control mb-3"
-                                 type="password"
-                                 id="passwordWelcome"
-                                 name="password"
-                              />
-                              <a
-                                 href="create-answer"
-                                 className="btn btn-success float-right"
-                              >
-                                 Log in
-                              </a>
-                           </form>
                         </div>
                      </div>
                   </div>
---
+++ src/ui/SignUp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default class SignUp extends React.Component {
+   render() {}
+}
