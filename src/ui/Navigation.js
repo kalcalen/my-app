8d69701b997567492d0b9ec894a0488974f1b92c
kalcalen <kalcalen@gmail.com>
2020-07-16 12:02:32 -0700

    added mock data

--- src/ui/Navigation.js
+++ src/ui/Navigation.js
@@ -2,13 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navigation() {
+   const url = window.location.pathname;
+   console.log(url);
+   const tabActiveOnPlayers = (url) => {
+      if (url.indexOf("players") > 0) {
+         return "tab-active";
+      } else return "";
+   };
+
    return (
       <div
          className="btn-group d-flex mt-1"
          role="navigation"
          aria-label="navigation"
       >
-         <Link type="players" className={"btn btn-secondary"}>
+         <Link
+            to="players"
+            className={`btn btn-secondary ${tabActiveOnPlayers(url)}`}
+         >
             Players
          </Link>
 
