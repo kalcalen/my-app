0e2c8af7139e959aef9baac2a91c1a00ff680b96
kalcalen <kalcalen@gmail.com>
2020-07-16 07:57:06 -0700

    created AppTemplate

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -1,48 +1,12 @@
 import React from "react";
-import Navigation from "../ui/Navigation";
+import AppTemplate from "../ui/AppTemplate";
 
 export default function Players() {
    return (
-      <div class="container">
-         <div class="row">
-            <div class="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
-               <Navigation />
-               <h4 class="my-4 text-center text-primary text-muted">
-                  Add memorable imagery
-               </h4>
+      <AppTemplate>
+         <h4 class="my-4 text-center text-primary text-muted">Players</h4>
 
-               <div class="mb-2">
-                  <div class="card bg-primary">
-                     <div class="card-body">
-                        <textarea rows="11" class="d-sm-none">
-                           {" "}
-                        </textarea>
-                        <textarea rows="6" class="d-none d-sm-block"></textarea>
-                     </div>
-                  </div>
-
-                  <div class="card bg-secondary">
-                     <div class="card-body"> </div>
-                  </div>
-               </div>
-
-               <p class="float-right mb-5">0/240</p>
-
-               <div class="clearfix"></div>
-
-               <button id="back2-answer" class="btn btn-link">
-                  Back to answer
-               </button>
-               <div class="float-right">
-                  <button
-                     class="btn btn-lg btn-primary float-right"
-                     id="save-imagery"
-                  >
-                     Save
-                  </button>
-               </div>
-            </div>
-         </div>
-      </div>
+         <div class="mb-2"></div>
+      </AppTemplate>
    );
 }
--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+import landingLogoBlk from "../img/landing-logo-blk.png";
+
 export default function Header() {
    return (
       <div>
-         {" "}
-         <img src={} width="32px;" alt="White Bear logo" />
-         <h3 class="d-inline text-brand">White Bear</h3>
-         <a href="index.html" class="btn btn-link float-right">
+         <img src={landingLogoBlk} alt="Who's Got Next" />
+
+         {/* <Link href="/" class="btn btn-link float-right">
             Log out
-         </a>
+         </Link> */}
          <div className="clearfix"></div>
       </div>
    );
