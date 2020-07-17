7b1c5a49774fd8145b60834d03d86a89476c501e
kalcalen <kalcalen@gmail.com>
2020-07-16 17:58:32 -0700

    cleaned up landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,55 +6,55 @@ export default function Landing() {
    return (
       <div className="background-image">
          <div className="container">
-            <div className="row content-center">
-               <div className="col-12">
-                  <img src={landingLogo} alt="" />
-               </div>
-            </div>
-
             <div className="row card-index">
                <div className="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                  <img src={landingLogo} alt="" />
                   <div id="landingCard" className="card mb-5">
                      <div className="card-body landing-text">
                         <h2 style={{ color: "white" }} className="card-title">
                            Nice to meet you
                         </h2>
-                        <Link to="sign-up" className="text-success mb-4">
-                           Not a member?
-                        </Link>
 
                         <div id="form-toggle">
-                           <label className="text-muted" for="email">
+                           <p
+                              style={{ color: "white" }}
+                              className=""
+                              for="email"
+                           >
                               Email Address
-                           </label>
+                           </p>
                            <input
                               className="form-control mb-3"
                               type="email"
                               id="emailCreate"
                               name="email"
                            />
-
-                           <p className="text-danger" id="emailError"></p>
-                           <label className="text-muted" for="password">
+                           <p
+                              style={{ color: "white" }}
+                              className="mt-5"
+                              for="password"
+                           >
                               Password
-                           </label>
+                           </p>
                            <input
-                              className="form-control mb-3 mt-3"
+                              className="form-control mb-3"
                               type="password"
                               id="passwordCreate"
                               name="password"
                            />
-                           <div
-                              className="text-danger"
-                              id="passwordError"
-                           ></div>
-                           <a
+                           <Link
                               id="letsGo"
-                              className="btn btn-success btn-block float-right mt-3"
+                              className="btn btn-success btn-block mt-3"
                               href="players"
                            >
                               Lets go!
-                           </a>
+                           </Link>
+                           <Link
+                              to="sign-up"
+                              className="btn btn-success btn-block mt-3"
+                           >
+                              Sign Up!
+                           </Link>
                         </div>
                      </div>
                   </div>
