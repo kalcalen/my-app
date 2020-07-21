b945c5acadb37e19badb75de8603786a6740b5c5
kalcalen <kalcalen@gmail.com>
2020-07-20 22:41:24 -0700

    fixed background on flip card

--- src/ui/UserDataBack.js
+++ src/ui/UserDataBack.js
@@ -6,8 +6,64 @@ const memoryCard = memoryCards[0];
 export default function UserDataBack() {
    return (
       <div className="card">
-         <div className="card-body bg-secondary">
+         <div className="card-body">
             <div rows="6" defaultValue={memoryCard.answer}></div>
+            <div rows="6">{memoryCard.availability.monday.am8.toString()}</div>
+            <div className="row">
+               <label className="col"></label>
+               <label className="col">M</label>
+               <label className="col">T</label>
+               <label className="col">W</label>
+               <label className="col">Th</label>
+               <label className="col">F</label>
+               <label className="col">Sa</label>
+               <label className="col">Su</label>
+            </div>
+            <div className="row" rows="6">
+               <label className="col">11am</label>
+               <input
+                  id="monAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.monday.am11}
+               />
+               <input
+                  id="tuesAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.tuesday.am11}
+               />
+               <input
+                  id="wedAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.wednesday.am11}
+               />
+               <input
+                  id="thursAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.thursday.am11}
+               />
+               <input
+                  id="friAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.friday.am11}
+               />
+               <input
+                  id="satAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.saturday.am11}
+               />
+               <input
+                  id="sunAm11"
+                  type="checkbox"
+                  className="col"
+                  checked={memoryCard.availability.sunday.am11}
+               />
+            </div>
          </div>
       </div>
    );
