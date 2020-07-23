f273aa58d6bb773dec7d14d999c36595750f9c65
kalcalen <kalcalen@gmail.com>
2020-07-23 01:19:37 -0700

    added more people to users.js

--- src/mock-data/users.js
+++ src/mock-data/users.js
@@ -11,4 +11,22 @@ const users = [
       password: "0003CBD9F276F3EFE8B94A796DEAF8216B411C31",
       createdAt: 1594337108,
    },
+   {
+      id: "455a2aa8-dce6-423a-8b1a-cd2f1550d576",
+      email: "timmy@yahoo.com",
+      password: "711F61297EBACA4335F3BEA2E8FED488",
+      createdAt: 1594337108,
+   },
+   {
+      id: "d1f64f17-3e1d-4f11-9434-1e5e546f5d14",
+      email: "dillon@yahoo.com",
+      password: "4BE06EFD940F6BB8051821CBE4540B6F",
+      createdAt: 1594337108,
+   },
+   {
+      id: "98f04cf9-80bc-454c-8b31-f4b21d972d63",
+      email: "christian@gmail.com",
+      password: "AA793501CA03735BFA034743F4659235",
+      createdAt: 1594337108,
+   },
 ];
